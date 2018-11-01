5afeabcb759c586b36f1b29bc98ed00ed9625bfb
Glen Sale and Patricia Filipovic <patricia@>
2018-11-01 10:48:15 -0700

    it will update

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,6 @@ var diceResult2 = function(num) {
   return dice;
 }
 
-
-
-
-
-
 //Buisness Logic For Player Options----
 function Players (turn){
   this.playername = " ";
@@ -67,36 +62,41 @@ Players.prototype.newgame= function (){
 }
 
 
+var diceRoll2;
+var diceRoll1;
 //User Interface Logic --
 $(document).ready(function() {
-
   //Dice Roller 1 ---
   $("#diceRoller1").click(function() {
-     var diceRoll1 = diceResult1(6);
+     diceRoll1 = diceResult1(6);
      $("#diceResult1").html("<img src='img/" +diceRoll1 + ".jpg'>");
+     $('#round-total1').text("Die 1 total=  " + diceRoll1);
     });
 
 //Dice Roller 1 ---
     $("#diceRoller2").click(function() {
-       var diceRoll2= diceResult2(6);
-       $("#diceResult2").html("<img src='img/" +diceRoll2 + ".jpg'>" + diceRoll2);
+       diceRoll2= diceResult2(6);
+       $("#diceResult2").html("<img src='img/" +diceRoll2 + ".jpg'>");
+       $('#round-total2').text("Die 2 total=  " + diceRoll2);
+
       });
 
-
-
-
-  });
 //input name --
-  $("form#name").submit(function(event) {
-    event.preventDefault();
-    var inputtedPlayerOne =$("input#name1").val();
-  //  console.log(inputtedPlayerOne);
-    var inputtedPlayerTwo=$("input#name2").val();
-    $("#name1").val("");
-    $("#name2").val("");
-    $(".output1").text("Player 1: " + (inputtedPlayerOne));
-    $(".output2").text("Player 2: " + (inputtedPlayerTwo));
+      $("form#name").submit(function(event) {
+        event.preventDefault();
+        var inputtedPlayerOne =$("input#name1").val();
+      //  console.log(inputtedPlayerOne);
+        var inputtedPlayerTwo=$("input#name2").val();
+        // $("#name1").val("");
+        // $("#name2").val("");
+        console.log(name1);
+        $(".output1").text("Player 1: " + (inputtedPlayerOne));
+        $(".output2").text("Player 2: " + (inputtedPlayerTwo));
+      });
+
   });
+
+
 
 
 
